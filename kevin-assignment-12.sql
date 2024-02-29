52d157694b12338378b00e3d357237f0e4bab10a
KevinGallaccio <gallacciokevin@gmail.com>
2024-02-29 12:17:33 +0100

    Update kevin-assignment-12.sql
    . Populated DB data from requirements

--- kevin-assignment-12.sql
+++ kevin-assignment-12.sql
@@ -39,3 +39,33 @@ CREATE TABLE IF NOT EXISTS `pizza` (
     FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`),
 	FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`pizza_id`)
   );
+  
+    
+-- Now let's start populating the data with the 3 orders given in the assignment:
+
+-- Inserting data into `customer` table:
+INSERT INTO `customer` (`customer_name`, `phone_number`) VALUES
+('Trevor Page', '226-555-4982'),
+('John Doe', '555-555-9498');
+
+-- Inserting data into `order` table:
+INSERT INTO `order` (`customer_id`, `order_date_time`) VALUES
+(1, '2014-09-10 09:47:00'), -- Order #1 from Trevor Page
+(2, '2014-09-10 13:20:00'), -- Order #2 from John Doe
+(1, '2014-09-10 09:47:00'); -- Order #3 from Trevor Page
+
+-- Inserting data into `pizza` table:
+INSERT INTO `pizza` (`pizza_name`, `pizza_price`) VALUES
+('Pepperoni & Cheese', 7.99),
+('Vegetarian', 9.99),
+('Meat Lovers', 14.99),
+('Hawaiian', 12.99);
+
+-- Inserting data into `pizza_order` table
+INSERT INTO `pizza_order` (`order_id`, `pizza_id`) VALUES
+(1, 1),  -- Order #1: 1x Pepperoni & Cheese Pizza
+(1, 3),  -- Order #1: 1x Meat Lovers Pizza
+(2, 2),  -- Order #2: 1x Vegetarian Pizza
+(2, 3),  -- Order #2: 2x Meat Lovers Pizza
+(3, 3),  -- Order #3: 1x Meat Lovers Pizza
+(3, 4);  -- Order #3: 1x Hawaiian Pizza
